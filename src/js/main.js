7009344c3208b911ffd63d42c9945c9bf961383e
EterNorSos <norbert.sosnowski@outlook.com>
2023-01-16 00:35:45 +0100

    Adding logic for Hamburger menu via Aria props

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,24 @@
 import "../css/style.scss";
-// import { setupCounter } from "../../counter";
 
-// setupCounter(document.querySelector("#counter"));
+document.addEventListener("DOMContentLoaded", (e) => {
+  function checkQueries() {
+    mediaQueryMobile = window.matchMedia(`(max-width: 426px)`).matches;
+    menu.ariaHidden = "" + mediaQueryMobile;
+    menu.ariaExpanded = mediaQueryMobile ? false : null;
+  }
+
+  const menu = document.querySelector(".navbar__hoverLink");
+
+  let mediaQueryMobile;
+  checkQueries();
+
+  window.addEventListener("resize", (e) => {
+    checkQueries();
+  });
+
+  document.querySelector("#hamburgerMenu").addEventListener("click", (e) => {
+    menu.classList.toggle("is-active");
+    menu.ariaHidden = menu.ariaHidden === "true" ? "false" : "true";
+    menu.ariaExpanded = menu.ariaExpanded === "true" ? "false" : "true";
+  });
+});
